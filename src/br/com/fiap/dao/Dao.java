0bff24102fc4d9bc38707c8c88d628930fca6619
RM48344 <RM48344@L2901MICRO11.fiap.com.br>
2016-05-04 21:12:04 -0300

    Intervalo de aula.

--- src/br/com/fiap/dao/Dao.java
+++ src/br/com/fiap/dao/Dao.java
@@ -1,7 +1,19 @@
 package br.com.fiap.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Dao {
 	
+	protected Connection cn;
+	protected PreparedStatement stmt;
+	protected ResultSet rs;
 	
+	private String url = "jdbc:mysql://localhost:3306/vendas";
+	
+	protected void abrirConexao(){
+		Class.forName("com.mysql.jdbc.Driver");
+	}
 	
 }
